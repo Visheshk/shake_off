f534efff391ef81ade350708c0a9ee58c2bb8042
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2021-11-28 07:34:49 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(score)
+})
+input.onButtonPressed(Button.AB, function () {
+    score = 0
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Shake")
+    for (let value of list) {
+        basic.showNumber(value)
+    }
+    basic.showNumber(score)
+})
+input.onGesture(Gesture.Shake, function () {
+    score += 1
+})
+let list: number[] = []
+let score = 0
+score = 0
+list = [
+10,
+9,
+8,
+7,
+6,
+5,
+4,
+3,
+2,
+1,
+0
+]
+basic.showString("Press A+B to start")
+basic.forever(function () {
+	
+})
