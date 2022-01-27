a15dbcb264aa1c498f50943e06bfacc6d937fb80
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-01-27 13:06:01 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,35 +2,24 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(score)
 })
 input.onButtonPressed(Button.AB, function () {
-    score = 0
+    index = 10
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
     basic.showString("Go")
-    for (let value of list) {
-        basic.showNumber(value)
+    score = 0
+    while (index > 0) {
+        basic.showNumber(index)
+        basic.pause(1000)
     }
     basic.showNumber(score)
 })
 input.onGesture(Gesture.Shake, function () {
     score += 1
 })
-let list: number[] = []
+let index = 0
 let score = 0
 score = 0
-list = [
-10,
-9,
-8,
-7,
-6,
-5,
-4,
-3,
-2,
-1,
-0
-]
 basic.showString("A+B to start")
 basic.forever(function () {
 	
