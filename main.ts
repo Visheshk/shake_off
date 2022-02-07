03418193352ba0df0ba99f6c4cd5c2d936c1cb1f
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-02-07 10:25:34 -0600

    refactored code to add a function

--- main.ts
+++ main.ts
@@ -3,26 +3,26 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     index = 3
-    for (let index2 = 0; index2 < 3; index2++) {
-        basic.showNumber(index)
-        basic.pause(500)
-        index += -1
-    }
+    countdown_timer(2)
     basic.showString("Go")
     score = 0
-    while (countdown > 0) {
-        basic.showNumber(countdown)
-        basic.pause(500)
-        countdown += -1
-    }
+    countdown_timer(9)
     basic.showNumber(score)
 })
 input.onGesture(Gesture.Shake, function () {
     score += 1
 })
-let index = 0
+function countdown_timer (num: number) {
+    countdown = num
+    while (countdown >= 0) {
+        basic.showNumber(countdown)
+        basic.pause(500)
+        countdown += -1
+    }
+    basic.clearScreen()
+}
 let countdown = 0
+let index = 0
 let score = 0
 score = 0
-countdown = 10
 basic.showString("A+B to start")
