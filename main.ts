2a4d8a326893f084780081d3748488d9c2f9037d
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-02-07 10:00:58 -0600

    Changed the code structure

--- main.ts
+++ main.ts
@@ -2,15 +2,18 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(score)
 })
 input.onButtonPressed(Button.AB, function () {
-    index = 10
-    basic.showNumber(3)
-    basic.showNumber(2)
-    basic.showNumber(1)
+    index = 3
+    for (let index2 = 0; index2 < 3; index2++) {
+        basic.showNumber(index)
+        basic.pause(500)
+        index += -1
+    }
     basic.showString("Go")
     score = 0
-    while (index > 0) {
-        basic.showNumber(index)
-        basic.pause(1000)
+    while (countdown > 0) {
+        basic.showNumber(countdown)
+        basic.pause(500)
+        countdown += -1
     }
     basic.showNumber(score)
 })
@@ -18,9 +21,8 @@ input.onGesture(Gesture.Shake, function () {
     score += 1
 })
 let index = 0
+let countdown = 0
 let score = 0
 score = 0
+countdown = 10
 basic.showString("A+B to start")
-basic.forever(function () {
-	
-})
