2d3a7fbc1e56ce5414ab7aac00ed82bb77d75384
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-05-16 21:52:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber((finish - start) / 1000)
+    if (done == 1) {
+        basic.showNumber((finish - start) / 1000)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     score += 1
-    if (score == max_shakes) {
-        finish = control.millis()
-        soundExpression.happy.playUntilDone()
-    }
     led.plotBarGraph(
     score,
     max_shakes
     )
+    if (score == max_shakes) {
+        soundExpression.happy.playUntilDone()
+        finish = control.millis()
+        done = 1
+    }
 })
-let max_shakes = 0
+let done = 0
 let finish = 0
 let start = 0
+let max_shakes = 0
 let score = 0
 basic.showString("shake")
 score = 0
+max_shakes = 40
 start = control.millis()
 finish = 0
-max_shakes = 40
+done = 0
